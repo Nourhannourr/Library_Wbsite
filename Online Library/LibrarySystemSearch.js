4eecfa72462ac0c5b1956e7cbe0f54c345e9f53e
aseel <aseelmohammed998@yahoo.com>
2025-04-22 15:26:09 +0200

    customized alert box

--- Online Library/LibrarySystemSearch.js
+++ Online Library/LibrarySystemSearch.js
@@ -1,23 +1,52 @@
+console.log("LibrarySystemSearch");
+//// alert box
+const showAlertBtn =document.getElementById('showAlert');
+const customAlert = document.getElementById('customAlert');
+const confirmBtn =document.getElementById('confirm');
+const goToList=document.getElementById('GoToList');
+
 let searchInput = document.getElementById('searchInput');
-//localStorage.setItem('book1', 'harry potter');
+// localStorage.setItem('book1', JSON.stringify({
+//     title: 'Harry Potter',
+//     author: 'J.K. Rowling',
+//     category: 'Fantasy',
+// }));
+console.log("added to localStorage");
 function search(value) {
+    searchInput.addEventListener('keyup',(e)=>{
+    })
     if(localStorage.length == 0){
-        console.log("clear")
+        console.log("empty")
         return;
     }
     let found=false;
     for (let i = 0; i < localStorage.length; i++) {
         const key = localStorage.key(i);
-        const item = localStorage.getItem(key);
-        if (item.toLowerCase().includes(value.toLowerCase())) {
-            alert("Book is Found in the system ,you can check the book list");
+        const item = JSON.parse(localStorage.getItem(key));
+        if (item.title.toLowerCase().includes(value.toLowerCase())) {
+            showCustomAlert();
             found = true;
             searchInput.innerHTML='';
             break;
         }
     }
         if(!found ) {
-            alert("sorry the book is not in stock");
+            text =document.getElementById('text').innerHTML='sorry the book is not in stock :(';
+            goToList.style.display = 'none';
+            showCustomAlert();
         }
     searchInput.value='';
 }
+function showCustomAlert() {
+    customAlert.style.display = 'flex';
+}
+
+confirmBtn.addEventListener('click', () => {
+    customAlert.style.display = 'none';
+});
+
+goToList.addEventListener('click', () => {
+    window.open('book_list.html');
+});
+
+
