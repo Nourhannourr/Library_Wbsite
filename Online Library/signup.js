3bd4cfe5fd5a08f69e8b64242b3014c493d44971
MohamedEssam004 <m.f.elgafy.6329@gmail.com>
2025-04-24 02:32:59 +0200

    Update signup.js

--- Online Library/signup.js
+++ Online Library/signup.js
@@ -7,6 +7,7 @@ form.addEventListener("submit", function (e) {
 
   let sname = document.getElementById("usname").value;
   let passw = document.getElementById("pass").value;
+  let emaiil = document.getElementById("eemaill").value;
 
   if (pass.value != conpas.value) {
     alert("Sorry, the confirmation of password is wrong, please try again");
@@ -15,7 +16,7 @@ form.addEventListener("submit", function (e) {
   }
 
   let users = JSON.parse(localStorage.getItem("users")) || [];
-  users.push({ username: sname, password: passw });
+  users.push({ username: sname, password: passw, email: emaiil});
   localStorage.setItem("users", JSON.stringify(users));
   window.location.href = "index.html";
 });
